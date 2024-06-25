f3acadbdf55f57f6cdb3c94b01aed7ad39d5b0ba
The Frederation <william@thewindinthewillows.net>
2024-06-25 23:31:58 +0100

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'dont-talk-sport-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/audio/background-music.mp3',
+    '/manifest.json' // Include other resources as needed
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
